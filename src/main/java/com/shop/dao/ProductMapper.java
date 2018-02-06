444d9e96bb2bead392cefc38f8c8841a73a10f06
dingmingzhe <15545171037@163.com>
2018-02-06 13:42:20 +0800

    init project

---
+++ src/main/java/com/shop/dao/ProductMapper.java
@@ -0,0 +1,19 @@
+package com.shop.dao;
+
+
+import com.shop.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+
+}
---
+++ src/main/java/com/shop/dao/ShippingMapper.java
@@ -0,0 +1,19 @@
+package com.shop.dao;
+
+import com.shop.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+
+
+}
